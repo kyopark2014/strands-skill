40eb4533ea5a2e25c46352fce7b327ea9c1b0f99
John Park <kyopark2014@gmail.com>
2026-04-16 16:19:01 +0900

    update

--- application/app.py
+++ application/app.py
@@ -29,7 +29,7 @@ os.environ["DEV"] = "true"  # Skip user confirmation of get_user_input
 config = utils.load_config()
 
 # title
-st.set_page_config(page_title='Strands Agent', page_icon=None, layout="centered", initial_sidebar_state="auto", menu_items=None)
+st.set_page_config(page_title='Strands Skill', page_icon=None, layout="centered", initial_sidebar_state="auto", menu_items=None)
 
 plugin_list = plugin.available_plugins_list()
 logger.info(f"plugin_list: {plugin_list}")
@@ -90,7 +90,7 @@ with st.sidebar:
     
     st.markdown(
         "Stands Agent SDK를 이용하여 효율적인 Agent를 구현합니다." 
-        "상세한 코드는 [Github](https://github.com/kyopark2014/strands-agent)을 참조하세요."
+        "상세한 코드는 [Github](https://github.com/kyopark2014/strands-skill)을 참조하세요."
     )
 
     st.subheader("🐱 대화 형태")
